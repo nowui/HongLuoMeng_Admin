d698056bd77d6e0da129ad2674faa1ab007c0bf4
NowUI <nowui@163.com>
2016-12-03 16:49:42 +0800

    2016-12-03 16:49

--- src/views/brand/apply/Index.jsx
+++ src/views/brand/apply/Index.jsx
@@ -155,7 +155,7 @@ class BrandApplyIndex extends Component {
                 </Row>
 
                 <div>
-                    <Table columns={columns} dataSource={this.state.list} pagination={pagination}/>
+                    <Table columns={columns} dataSource={this.state.list} pagination={pagination} bordered/>
                 </div>
             </div>
         )
